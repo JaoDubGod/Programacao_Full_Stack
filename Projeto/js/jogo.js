686cb638aa5ef29c52866cf08ae2a1a7b54eb0af
JaoDubGod <joaopsmonti@yahoo.com>
2025-05-08 18:55:40 -0300

    Update jogo.js

--- Projeto/js/jogo.js
+++ Projeto/js/jogo.js
@@ -1,5 +1,5 @@
 const scoreDisplay = document.getElementById('pontos') // Criando a constante de pontos
-const width = 38 // Definindo a quantidade de colunas
+const width = 40 // Definindo a quantidade de colunas
 let pontos = 0 // Declarando os pontos sendo zero
 const game = document.querySelector('.game') // Selecionando o primeiro elemento DOM de um seletor CSS especificado
 
@@ -32,7 +32,7 @@ const layout = [
     1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1,
     1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
     1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
-] // Layout criado com 38 colunas (38x38)
+] // Layout criado com 40 colunas (40x40)
 
 // 0 - pac-dots (pontos)
 // 1 - parede
@@ -40,14 +40,29 @@ const layout = [
 // 3 - power-pellet (circulos maiores que deixam fantasmas vulneráveis)
 // 4 - espaço vazio
 
-const quadrado = [] // Definindo a constante quadrado e criando um array vazio
+const quadrados = [] // Definindo a constante quadrado e criando um array vazio
 
 // Criando o tabuleiro
 function createTabu() {
-    // Laço que irá percorrer todos os 784 elementos de layout
+    // Laço que irá percorrer todos os 1600 elementos de layout
     for (let i = 0; i < layout.length; i++) {
         const quadrado = document.createElement('div') // Criando a div que representa uma célula no tabuleiro
         game.appendChild(quadrado) // Colocando o quadrado (square) dentro da grade (game)
+        quadrados.push(quadrado) // Adicionando um item no finao do array
+
+        // Adicionando o layout para o tabuleiro
+        if (layout[i] === 0) {
+            quadrados[i].classList.add("pac-dot")
+        } // Se no layout for 0, será um pac-dot
+        if (layout[i] === 1) {
+            quadrados[i].classList.add("parede")
+        } // Se no layout for 1, será uma parede
+        if (layout[i] === 2) {
+            quadrados[i].classList.add("covil-fantasma")
+        } // Se no layout for 2, será o covil dos fantasmas
+        if (layout[i] === 3) {
+            quadrados[i].classList.add("power-pellet")
+        } // Se no layout for 4, será um power-pellet
     }
 }
 createTabu() // Chamando a função para construir o tabuleiro
