6aaa65c70d2fdafab8082999021d86d82149026c
JaoDubGod <joaopsmonti@yahoo.com>
2025-05-16 20:06:03 -0300

    Atualizar o servidor.js

--- Pratica/LAB_7/projeto/servidor.js
+++ Pratica/LAB_7/projeto/servidor.js
@@ -1,131 +1,138 @@
 require("colors");
 var http = require("http");
 var express = require("express");
-var bodyParser = require("body-parser")
+var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 
 const MongoClient = mongodb.MongoClient;
 const uri = `mongodb+srv://unifjmonti:HtKXKkuUL2AiZKRj@tentativa.kbjzkcs.mongodb.net/?retryWrites=true&w=majority&appName=Tentativa`;
 const client = new MongoClient(uri);
 
-var dbo = client.db("exemplo_bd");
-var usuarios = dbo.collection("usuarios");
+async function iniciarServidor() {
+    try {
+        await client.connect();
+        console.log("Conectado ao MongoDB!".green);
 
-var app = express();
-app.use(express.static('./public'))
-app.use(bodyParser.urlencoded({extended: false }))
-app.use(bodyParser.json())
-app.set('view engine', 'ejs')
-app.set('views', './views');
+        const dbo = client.db("exemplo_bd");
+        const usuarios = dbo.collection("usuarios");
 
-var server = http.createServer(app);
-server.listen(80);
+        var app = express();
+        app.use(express.static('./public'));
+        app.use(bodyParser.urlencoded({ extended: false }));
+        app.use(bodyParser.json());
+        app.set('view engine', 'ejs');
+        app.set('views', './views');
 
-console.log("Servidor rodando ...".rainbow);
+        // Rotas
 
+        app.get("/inicio", function(req, res){
+            res.redirect("../Aula_1/projects.html");
+        });
 
-// Métodos e actions
+        app.post("/inicio", function(req, res){
+            res.redirect("../Aula_1/projects.html");
+        });
 
-app.get("/inicio", function(requisicao, resposta){
-    resposta.redirect("../Aula_1/projects.html")
-})
+        app.get("/cadastrar", function(req, res){
+            let nome = req.query.nome;
+            let login = req.query.login;
+            let senha = req.query.senha;
+            let nasc = req.query.nascimento;
 
-app.post("/inicio", function(requisicao, resposta){
-    resposta.redirect("../Aula_1/projects.html")
-})
+            console.log(nome, login, senha, nasc);
+        });
 
-app.get("/cadastrar", function(requisicao, resposta){
-    let nome = requisicao.query.nome;
-    let login = requisicao.query.login;
-    let senha = requisicao.query.senha;
-    let nasc = requisicao.query.nascimento;
+        app.post("/cadastrar", function(req, res){
+            let nome = req.body.nome;
+            let login = req.body.login;
+            let senha = req.body.senha;
+            let nasc = req.body.nascimento;
+            console.log(nome, login, senha, nasc);
 
-    console.log(nome, login, senha, nasc)
-})
+            var data = { db_nome: nome, db_login: login, db_senha: senha, db_nasc: nasc };
 
-app.post("/cadastrar", function(requisicao, resposta){
-    let nome = requisicao.body.nome;
-    let login = requisicao.body.login;
-    let senha = requisicao.body.senha;
-    let nasc = requisicao.body.nascimento;
-    console.log(nome, login, senha, nasc);
+            usuarios.insertOne(data, function(err){
+                if(err){
+                    res.render("resposta",{status: "Erro", nome, login, senha, nasc});
+                } else {
+                    res.render("resposta",{status: "Sucesso", nome, login, senha, nasc});
+                }
+            });
+        });
 
-    var data = { db_nome: nome, db_login: login, db_senha: senha, db_nasc: nasc };
+        app.get("/for_ejs",function(req, res){
+            let valor = req.query.valor;
+            res.render("divs", { valor });
+        });
 
-    usuarios.insertOne(data, function(err){
-        if(err){
-            resposta.render("resposta",{status: "Erro" ,nome, login, senha, nasc});
-        }else{
-            resposta.render("resposta",{status: "Sucesso", nome, login, senha, nasc});
-        }
-    })
-})
+        app.post('/login', function(req, res){
+            let login = req.body.login;
+            let senha = req.body.senha;
+            console.log(login, senha);
 
-app.get("/for_ejs",function(requisicao, resposta){
-    let valor = requisicao.query.valor;
-    resposta.render("divs",{valor});
-})
+            var data = { db_login: login, db_senha: senha };
 
-app.post('/login', function(requisicao, resposta){
-    let login = requisicao.body.login;
-    let senha = requisicao.body.senha;
-    console.log(login, senha);
+            usuarios.find(data).toArray(function(err, items){
+                console.log(items);
+                if (err) {
+                    res.render("resposta_login", { status: "erro ao logar" });
+                } else if (items.length === 0) {
+                    res.render("resposta_login", { status: "usuario/senha não encontrado" });
+                } else {
+                    res.render("resposta_login", { status: "usuario " + login + " logado" });
+                }
+            });
+        });
 
-    var data = {db_login: login, db_senha: senha}
+        app.post('/atualizar_senha', function(req, res){
+            let login = req.body.login;
+            let senha = req.body.senha;
+            let novasenha = req.body.novasenha;
 
-    usuarios.find(data).toArray(function(err, items){
-        console.log(items)
-        if(items.length == 0){
-            resposta.render("resposta_login",{status: "usuario/senha não encontrado"});
-        }else if(err){
-            resposta.render("resposta_login",{status: "erro ao logar"});
-        }else{
-            resposta.render("resposta_login",{status: "usuario "+login+" logado"});
-        }
-    })
+            let data = { db_login: login, db_senha: senha };
+            let new_data = { $set: { db_senha: novasenha } };
 
-})
+            usuarios.updateOne(data, new_data, function(err, result){
+                console.log(result);
 
+                if (err) {
+                    res.render('resposta_login', { status: "Erro ao atualizar usuário!" });
+                } else if (result.modifiedCount == 0) {
+                    res.render('resposta_login', { status: "Usuário/senha não encontrado!" });
+                } else {
+                    res.render('resposta_login', { status: "Usuário atualizado com sucesso!" });
+                }
+            });
+        });
 
-app.post('/atualizar_senha', function(requisicao, resposta){
-    let login = requisicao.body.login;
-    let senha = requisicao.body.senha;
-    let novasenha = requisicao.body.novasenha;
+        app.post('/remover_usuario', function(req, res){
+            let login = req.body.login;
+            let senha = req.body.senha;
 
-    let data = { db_login: login, db_senha: senha }
-    let new_data = { $set: {db_senha: novasenha}}
+            let data = { db_login: login, db_senha: senha };
 
-    usuarios.updateOne(data, new_data, function(err, result){
-        console.log(result);
-    
-        if (result.modifiedCount == 0) {
-            resposta.render('resposta_login', {status: "Usuário/senha não encontrado!"})
-        }else if (err) {
-            resposta.render('resposta_login', {status: "Erro ao atualizar usuário!"})
-        }else {
-            resposta.render('resposta_login', {status: "Usuário atualizado com sucesso!"})        
-        };
-    })
+            usuarios.deleteOne(data, function(err, result){
+                console.log(result);
 
-})
+                if (err) {
+                    res.render('resposta_login', { status: "Erro ao remover usuário!" });
+                } else if (result.deletedCount == 0) {
+                    res.render('resposta_login', { status: "Usuário/senha não encontrado!" });
+                } else {
+                    res.render('resposta_login', { status: "Usuário removido com sucesso!" });
+                }
+            });
+        });
 
+        // Inicia o servidor na porta 80
+        var server = http.createServer(app);
+        server.listen(80, () => {
+            console.log("Servidor rodando ...".rainbow);
+        });
 
+    } catch (err) {
+        console.error("Erro ao conectar ao MongoDB:", err);
+    }
+}
 
-app.post('/remover_usuario', function(requisicao, resposta){
-    let login = requisicao.body.login;
-    let senha = requisicao.body.senha;
-
-    let data = { db_login: login, db_senha: senha }
-
-    usuarios.deleteOne(data, function(err, result){
-        console.log(result);
-
-          if (result.deletedCount == 0) {
-            resposta.render('resposta_login', {status: "Usuário/senha não encontrado!"})
-          }else if (err) {
-            resposta.render('resposta_login', {status: "Erro ao remover usuário!"})
-          }else {
-            resposta.render('resposta_login', {status: "Usuário removido com sucesso!"})        
-          };
-    })
-})
+iniciarServidor();
